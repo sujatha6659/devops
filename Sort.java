53127c9861055f63c20f83a8879c832580a89ede
sujatha6659 <96616971+sujatha6659@users.noreply.github.com>
2024-06-04 10:17:13 +0530

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -1,27 +1,22 @@
+package programs;
+
 import java.util.Scanner;
-class Sort
-{
-    public static void main(String args[])
-    {
-        int s,t,i,j;
-        Scanner sc=new Scanner(System.in);
-        int a[]=new int[5];
-        System.out.print("enter the number:");
-        for(i=0;i<5;i++)
-        a[i]=sc.nextInt();
-        for(i=4;i>0;i--)
-        {
-            for(j=0;j<i;j++)
-            {
-                if(a[j+1]<a[j])
-                {
-                    s=a[j+1];
-                    a[j+1]=a[j];
-                    a[j]=s;
-                }
-            }
-        }
-        for(t=0;t<5;t++)
-        System.out.print(a[t]);
-    }
+
+public class Sort {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			String s = sc.nextLine();
+			int i = 0,j=s.length()-1,c=0;
+			while(i<j) {
+				if(s.charAt(i)!=s.charAt(j)) c+=1;
+				i++;
+				j--;
+			}
+		if(c==0)
+			System.out.println("Palindrome");
+		else 
+			System.out.println("Not Palindrome");
+	}
+
 }
