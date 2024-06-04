28d581756719bd0daa7749c62d149c2e1bd75fd2
sujatha6659 <96616971+sujatha6659@users.noreply.github.com>
2024-06-04 12:08:10 +0530

    first change

--- Sort.java
+++ Sort.java
@@ -1,22 +1,17 @@
-package programs;
-
 import java.util.Scanner;
-
-public class Sort {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-			String s = sc.nextLine();
-			int i = 0,j=s.length()-1,c=0;
-			while(i<j) {
-				if(s.charAt(i)!=s.charAt(j)) c+=1;
-				i++;
-				j--;
-			}
-		if(c==0)
-			System.out.println("Palindrome");
-		else 
-			System.out.println("Not Palindrome");
-	}
-
+ public class Student{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter firstname");
+        String fn=sc.nextLine();
+        System.out.println("enter lastname");
+        String ln=sc.nextLine();
+        System.out.println("hi "+fn+' '+ln);
+        System.out.println("enter your details:");
+        System.out.println("email");
+        String em=sc.nextLine();
+        System.out.println("enter phone no:");
+        int ph=sc.nextInt();
+        System.out.println("your email id is: "+em+" and phone number is:"+ph);
+    }
 }
