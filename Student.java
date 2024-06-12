ac2296fb5474afa66b30afb8e90c85ab4de59800
sujatha6659 <96616971+sujatha6659@users.noreply.github.com>
2024-06-12 09:38:45 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,15 +3,10 @@ import java.util.Scanner;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("enter firstname");
-        String fn=sc.nextLine();
         System.out.println("enter lastname");
-        String ln=sc.nextLine();
-        System.out.println("hi "+fn+' '+ln);
         System.out.println("enter your details:");
         System.out.println("email");
-        String em=sc.nextLine();
         System.out.println("enter phone no:");
-        int ph=sc.nextInt();
-        System.out.println("your email id is: "+em+" and phone number is:"+ph);
+        System.out.println("your email id is: ");
     }
 }
